96fa3f12b617606090610047420cfdff220fd255
ritikgit <63956562+ritikgit@users.noreply.github.com>
2020-06-01 20:26:54 +0530

    Update aria.sh

--- aria.sh
+++ aria.sh
@@ -1,7 +1,10 @@
 export MAX_DOWNLOAD_SPEED=0
-export MAX_CONCURRENT_DOWNLOADS=3
+export MAX_CONCURRENT_DOWNLOADS=12
+tracker_list=curl -Ns https://raw.githubusercontent.com/ngosang/trackerslist/master/trackers_all.txt | awk '$1' | tr '\n' ',' | cat export tracker="[$tracker_list]"
+
 aria2c --enable-rpc --rpc-listen-all=false --rpc-listen-port 6800 \
   --max-connection-per-server=10 --rpc-max-request-size=1024M \
   --seed-time=0.01 --min-split-size=10M --follow-torrent=mem --split=10 \
    --daemon=true --allow-overwrite=true --max-overall-download-limit=$MAX_DOWNLOAD_SPEED \
    --max-overall-upload-limit=1K --max-concurrent-downloads=$MAX_CONCURRENT_DOWNLOADS
+--bt-tracker=$trackers
